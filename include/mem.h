e31be6300864aa1bb64ac70aa938adefe7a391f8
felix <ichters.fe@gmail.com>
2026-07-08 01:03:48 +0200

    mem: define memory struct and basic functions

---
+++ include/mem.h
@@ -0,0 +1,18 @@
+#ifndef MEM_H
+#define MEM_H
+
+#include<stdlib.h>
+#include <stdint.h>
+
+struct memory {
+	uint8_t *data;
+	size_t size;
+};
+
+struct memory mem_create(size_t size);
+void mem_destroy(struct memory *m);
+
+uint8_t mem_read(struct memory *m, int addr);
+void mem_write(struct memory *m, int addr, uint8_t val);
+
+#endif
